0a71342ce961b1a0efe0768ee6a7839f0785a201
jasleen17-code <manvinderkaur359@gmail.com>
2026-03-03 16:48:48 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,51 @@
+var a = 100; var b = 200; var c = 300; var linebreak ="<br>";
+document.write("a+b+c=");
+result =a+b+c;
+document.write(result);
+document.write(linebreak);
+document.write("a*b*c=");
+result =a*b*c;
+document.write(result);     
+document.write(linebreak);
+document.write("a/b/c=");
+result =a/b/c;  
+document.write(result);
+document.write(linebreak);
+document.write("a%b%c=");
+result =a%b%c;  
+document.write(result); 
+document.write(linebreak);
+/**********Airthmatic operators in JavaScript**********/
+var a=10; var b=20; linebreak = "<br>";
+document.write("(a==b) = ");
+result = (a==b);
+document.write(result);
+document.write(linebreak);
+document.write("(a!=b) = ");
+result = (a!=b);
+document.write(result);     
+document.write(linebreak);
+document.write("(a>b) = ");
+result = (a>b);  
+document.write(result);
+document.write(linebreak);
+document.write("(a<b) = ");
+result = (a<b);         
+document.write(result); 
+document.write(linebreak);
+document.write("(a>=b) = ");
+result = (a>=b);
+document.write(result);
+document.write(linebreak);
+document.write("(a<=b) = ");
+result = (a<=b);  
+document.write(result);
+document.write(linebreak);
+/**********Relational operators in JavaScript**********/
+var count;
+document.write("Starting loop" +"<br>");
+for(count=1; count<=10; count++){
+    document.write("Current count = " + count + "<br>");
+}
+document.write("Loop ended");
+/**********Loops in JavaScript**********/
