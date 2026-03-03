7a9fb3d20e9893284d35b9e2e13ceccb4df0d953
jasleen17-code <manvinderkaur359@gmail.com>
2026-03-03 16:35:19 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,27 @@
+console.log("Hello World!");
+ var num = 10;
+console.log(num);
+ console.log(typeof num);
+var num = "Hello World!";
+ console.log(num);
+ console.log(typeof num); 
+num = false;
+ console.log(num);
+ console.log(typeof num);
+ num = null;
+console.log(num);
+ console.log(typeof num);
+ num = undefined;
+ console.log(num);
+console.log(typeof num)
+ /**********Data Types in JavaScript**********/
+ const sym1 = Symbol(4);
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if(sym1 === sym2){
+    console.log("True");
+}
+else{    console.log("False");
+}
+ /**********Objects in JavaScript**********/ 
